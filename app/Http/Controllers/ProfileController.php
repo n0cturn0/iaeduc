3d7b0776ad8389860863ea1fc255fd57450d2e42
Luiz Augusto <n0cturn0.debian@gmail.com>
2024-10-23 16:14:36 -0400

    Vai leleco

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+
+class ProfileController extends Controller
+{
+
+
+    public function upload (Request $request)
+    {
+        $request->validate([
+            'file' => ['required', 'file',  'mimes:pdf'],
+        ]);
+
+        if ($path =$request->file('file')->store('uploads', 'public'))
+        {
+            Log::info('Arquivo'. $path);
+            return back()->with('success', 'Arquivo enviado com sucesso')->with('file', $path);
+
+        } else {
+            log::error('Algo deu errado');
+        }
+
+
+
+    }
+    /**
+     * Display the user's profile form.
+     */
+    public function edit(Request $request): View
+    {
+        return view('profile.edit', [
+            'user' => $request->user(),
+        ]);
+    }
+
+    /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+    }
+
+    /**
+     * Delete the user's account.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        $request->validateWithBag('userDeletion', [
+            'password' => ['required', 'current_password'],
+        ]);
+
+        $user = $request->user();
+
+        Auth::logout();
+
+        $user->delete();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return Redirect::to('/');
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+                    @if(@session('success'))
+                    <div class="text-green-600">
+                        <strong>{{ session('success') }}</strong>
+                    </div>
+                        
+                    @endif
+                <!-- Formulário para envio de arquivo -->
+                <div class="p-6">
+                    <form action="{{ route('upload.file') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-4">
+                            <label for="file" class="block text-sm font-medium text-gray-700">Escolha um arquivo</label>
+                            <input type="file" id="file" name="file" required class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+                        </div>
+                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-blue-600 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition">Enviar</button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
