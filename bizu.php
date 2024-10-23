f905480286ebff758d3f298984c471872a6de76b
Luiz Augusto <n0cturn0.debian@gmail.com>
2024-10-23 16:27:37 -0400

    Bizu

---
+++ bizu.php
@@ -0,0 +1,11 @@
+Depois de clonar
+Criar base de dados
+./vendor/bin/sail artisan migrate
+Inicia o projeto
+./vendor/bin/sail up -d
+
+localhost/register 
+Se registra
+localhost/login
+acessa o Painel
+
